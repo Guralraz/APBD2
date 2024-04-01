0cf0df449f790a748defd7f2d75f6b261de53caa
Guralraz <87212802+Guralraz@users.noreply.github.com>
2024-04-01 16:26:32 +0200

    0001

---
+++ ContainerLoader/Containers/Container.cs
@@ -0,0 +1,27 @@
+﻿public abstract class Container
+{
+    public double LoadMass { get; protected set; }
+    public int Height { get; protected set; }
+    public double TareWeight { get; protected set; } // Container's own weight
+    public int Depth { get; protected set; }
+    public string SerialNumber { get; protected set; }
+    public double MaxLoadCapacity { get; protected set; }
+
+    protected Container(double loadMass, int height, double tareWeight, int depth, string serialNumber, double maxLoadCapacity)
+    {
+        LoadMass = loadMass;
+        Height = height;
+        TareWeight = tareWeight;
+        Depth = depth;
+        SerialNumber = serialNumber;
+        MaxLoadCapacity = maxLoadCapacity;
+    }
+
+    public abstract void Load(double mass);
+    public abstract void Unload();
+}
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) { }
+}
--- ContainerLoader/IHazardNotifier.cs
+++ ContainerLoader/IHazardNotifier.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ContainerLoader
+﻿public interface IHazardNotifier
 {
-    internal interface IHazardNotifier
-    {
-    }
-}
+    void NotifyHazard(string message);
+}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxgit helpxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
